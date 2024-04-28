f6d96706a79dc825abf5b0c65c26980c2efe4a64
hery <hery.ae@icloud.com>
2024-04-28 22:54:21 +0700

    fix lint

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -8,23 +8,21 @@ module('Unit | Serializer | application', function (hooks) {
   // Replace this with your real tests.
   let models = ['customer', 'menu-item', 'order', 'menu-item-order'];
 
-  models.forEach( function(item) {
+  models.forEach(function (item) {
     test('it exists', function (assert) {
       let store = this.owner.lookup('service:store');
       let serializer = store.serializerFor(item);
-  
+
       assert.ok(serializer);
     });
-  
+
     test('it serializes records', function (assert) {
       let store = this.owner.lookup('service:store');
       let record = store.createRecord(item, {});
-  
+
       let serializedRecord = record.serialize();
-  
+
       assert.ok(serializedRecord);
     });
-
   });
-
 });
