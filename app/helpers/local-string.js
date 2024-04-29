c4a1ea5c05a98e95aa469b74c8989d0ab35a16ef
hery <hery.ae@icloud.com>
2024-04-29 12:30:57 +0700

    npm test

--- app/helpers/local-string.js
+++ app/helpers/local-string.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function thousandsSeparator(positional /*, named*/) {
+export default helper(function localString(positional /*, named*/) {
   if (Number(positional[0])) {
     return Number(positional[0]).toLocaleString();
   }
--- tests/integration/helpers/local-string-test.js
+++ tests/integration/helpers/local-string-test.js
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'botdistrikt-orders/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | thousandsSeparator', function (hooks) {
+module('Integration | Helper | local-string', function (hooks) {
   setupRenderingTest(hooks);
 
   // TODO: Replace this with your real tests.
   test('it renders', async function (assert) {
     this.set('inputValue', '1234');
 
-    await render(hbs`{{thousands-separator this.inputValue}}`);
+    await render(hbs`{{local-string this.inputValue}}`);
 
     assert.dom().hasText('1,234');
   });
